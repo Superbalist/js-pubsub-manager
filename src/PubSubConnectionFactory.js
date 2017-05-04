5b59c9392c8754bfdebd8e382e044c339e3d6dc1
Matthew Goslett <matthew@goslett.co.za>
2017-05-04 16:43:27 +0200

    add .eslintrc, lint fixes + jsdoc documentation

--- src/PubSubConnectionFactory.js
+++ src/PubSubConnectionFactory.js
@@ -1,14 +1,39 @@
-"use strict";
+'use strict';
 
-var pubsub = require('@superbalist/js-pubsub');
-var DevNullPubSubAdapter = pubsub.DevNullPubSubAdapter;
-var LocalPubSubAdapter = pubsub.LocalPubSubAdapter;
-var redis = require('redis');
-var RedisPubSubAdapter = require('@superbalist/js-pubsub-redis');
-var googleCloudPubSub = require('@google-cloud/pubsub');
-var GoogleCloudPubSubAdapter = require('@superbalist/js-pubsub-google-cloud');
+let pubsub = require('@superbalist/js-pubsub');
+let DevNullPubSubAdapter = pubsub.DevNullPubSubAdapter;
+let LocalPubSubAdapter = pubsub.LocalPubSubAdapter;
+let redis = require('redis');
+let RedisPubSubAdapter = require('@superbalist/js-pubsub-redis');
+let googleCloudPubSub = require('@google-cloud/pubsub');
+let GoogleCloudPubSubAdapter = require('@superbalist/js-pubsub-google-cloud');
 
+/**
+ * PubSubConnectionFactory Class
+ *
+ * @example
+ * let pubSubManager = require('@superbalist/js-pubsub-manager');
+ * let PubSubConnectionFactory = pubSubManager.PubSubConnectionFactory;
+ *
+ * let factory = new PubSubConnectionFactory();
+ */
 class PubSubConnectionFactory {
+  /**
+   * Factory an adapter with the given config.
+   *
+   * @param {string} driver
+   * @param {Object} [config={}]
+   * @return {module:pubsub.PubSubAdapterInterface}
+   * @throws Error if the driver is not supported
+   * @example
+   * let config = {
+   *   'driver': 'redis',
+   *   'host': 'localhost',
+   *   'port': '6379',
+   *   'db': 0,
+   * }
+   * let adapter = factory.make('redis', config);
+   */
   make(driver, config = {}) {
     switch (driver) {
       case '/dev/null':
@@ -23,11 +48,25 @@ class PubSubConnectionFactory {
     throw new Error(`The driver [${driver}] is not supported.`);
   }
 
+  /**
+   * Create a RedisPubSubAdapter with the given config.
+   *
+   * @param {RedisAdapterConfig} config
+   * @return {RedisPubSubAdapter}
+   * @private
+   */
   _makeRedisAdapter(config) {
     let client = redis.createClient(config);
     return new RedisPubSubAdapter(client);
 }
 
+  /**
+   * Create a GoogleCloudPubSubAdapter with the given config.
+   *
+   * @param {GoogleCloudAdapterConfig} config
+   * @return {GoogleCloudPubSubAdapter}
+   * @private
+   */
   _makeGoogleCloudAdapter(config) {
     let clientConfig = {
       'projectId': config.project_id,
--- src/config.js
+++ src/config.js
@@ -1,5 +1,47 @@
-"use strict";
+'use strict';
 
+/**
+ * @typedef {Object} ManagerConfig
+ * @property {string} [default=process.env.PUBSUB_CONNECTION || 'REDIS']
+ * @property {Object} [connections]
+ * @property {DevNullAdapterConfig} connections./dev/null
+ * @property {LocalAdapterConfig} connections.local
+ * @property {RedisAdapterConfig} connections.redis
+ * @property {GoogleCloudAdapterConfig} connections.gcloud
+ */
+
+/**
+ * @typedef {Object} DevNullAdapterConfig
+ * @property {string} [driver=/dev/null]
+ */
+
+/**
+ * @typedef {Object} LocalAdapterConfig
+ * @property {string} [driver=local]
+ */
+
+/**
+ * @typedef {Object} RedisAdapterConfig
+ * @property {string} [driver=redis]
+ * @property {string} [host=process.env.REDIS_HOST || 'localhost']
+ * @property {number} [port=process.env.REDIS_PORT || 6379]
+ * @property {number} [db=0]
+ */
+
+/**
+ * @typedef {Object} GoogleCloudAdapterConfig
+ * @property {string} [driver=gcloud]
+ * @property {string} [project_id=process.env.GOOGLE_CLOUD_PROJECT_ID]
+ * @property {string} [key_file=process.env.GOOGLE_APPLICATION_CREDENTIALS]
+ * @property {?string} [client_identifier]
+ * @property {boolean} [auto_create_topics=true]
+ * @property {boolean} [auto_create_subscriptions=true]
+ */
+
+/**
+ *
+ * @type {ManagerConfig}
+ */
 let config = {
   /*
   |--------------------------------------------------------------------------
@@ -27,18 +69,18 @@ let config = {
   'connections': {
 
     '/dev/null': {
-      'driver': '/dev/null'
+      'driver': '/dev/null',
     },
 
     'local': {
-      'driver': 'local'
+      'driver': 'local',
     },
 
     'redis': {
       'driver': 'redis',
       'host': process.env.REDIS_HOST || 'localhost',
       'port': process.env.REDIS_PORT || 6379,
-      'db': 0
+      'db': 0,
     },
 
     'gcloud': {
@@ -47,8 +89,8 @@ let config = {
       'key_file': process.env.GOOGLE_APPLICATION_CREDENTIALS,
       'client_identifier': null,
       'auto_create_topics': true,
-      'auto_create_subscriptions': true
-    }
+      'auto_create_subscriptions': true,
+    },
   },
 };
 
